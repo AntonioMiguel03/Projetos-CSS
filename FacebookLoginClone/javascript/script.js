f6156e1c8ff5c1058d16e6867403f23b731e3942
Antônio Miguel <88382914+AntonioMiguel03@users.noreply.github.com>
2024-01-03 14:35:01 -0300

    Add files via upload

---
+++ FacebookLoginClone/javascript/script.js
@@ -0,0 +1,11 @@
+function btnIniciar() {
+    alert("Loga com a conta se os dados estiverem corretos!");
+}
+
+function btnEsqueceuSenha() {
+    alert("Vai para a tela de recuperação de senha!");
+}
+
+function btnCadastrar() {
+    alert("Vai para o formulário de cadastro!");
+}
---
+++ FacebookLoginClone/readme.md
@@ -0,0 +1,3 @@
+# Descrição 
+
+Clone de uma página de cadstro e login do Facebook utilzando CSS.
